5f0ec280fb7f5d3b7204229b68a0d029a21621f5
Pat <proach@gap.com>
2019-10-17 16:46:15 -0400

    asdjksadj

--- src/packages/QuestionSection/questionSection.spec.js
+++ src/packages/QuestionSection/questionSection.spec.js
@@ -32,6 +32,7 @@ describe("<questionSection />", () => {
     it("renders without crashing", () => {
     shallow(<QuestionSection {...mockProps}/>);
     });
+    
     it("should fire the highOrLow helper on mounting", () => {
     shallow (<QuestionSection {...mockProps} />);
     expect(spyOnHighOrLow.mock.calls.length).toBe(1);
